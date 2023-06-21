31dbfa1be3119d914d55438b7c4d18f9e265933b
DanyPlaya <danila.lusuy@gmail.com>
2023-06-21 18:10:37 +0300

    update

--- config/build/buildLoders.ts
+++ config/build/buildLoders.ts
@@ -3,14 +3,6 @@ import { BuildOptions } from './types/config';
 import { buildCssLoaders } from './loaders/buildCssLoaders';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-        use: [
-            {
-                loader: 'file-loader',
-            },
-        ],
-    };
     const babelLoader = {
         test: /\.(js|jsx|tsx)$/,
         exclude: /node_modules/,
@@ -29,6 +21,14 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                 ],
             },
         },
+    };
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
     };
     const svgLoader = {
         test: /\.svg$/,
